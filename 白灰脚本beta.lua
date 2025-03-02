973490ba5ec6ca40a8c6a67d99b59e54937cddcb
CloudX-ScriptsWane <136167097+CloudX-ScriptsWane@users.noreply.github.com>
2025-03-02 18:07:44 +0800

    Update 白灰脚本beta.lua

--- 白灰脚本beta.lua
+++ 白灰脚本beta.lua
@@ -1,1 +1,6 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "通知",
+    Text = "可惜的云脚本\nXiaoYunCN已消失！",
+    Duration = 14.4
+})
 loadstring(game:HttpGet("https://github.com/CloudX-ScriptsWane/White-ash-script/raw/main/qwq", true))()
