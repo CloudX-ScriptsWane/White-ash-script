b97d95f19c34ef7f54c59ef8d56520efd5dbe113
CloudX-ScriptsWane <136167097+CloudX-ScriptsWane@users.noreply.github.com>
2025-03-03 14:42:02 +0800

    Update 白灰脚本beta.lua

--- 白灰脚本beta.lua
+++ 白灰脚本beta.lua
@@ -1,6 +1,6 @@
 game.StarterGui:SetCore("SendNotification", {
     Title = "通知",
-    Text = "好多功能没了\n那就别再使用云脚本\n推荐使用XAHub脚本",
+    Text = "好多功能没了\n那就别再使用云脚本",
     Duration = 14.4
 })
 game.StarterGui:SetCore("SendNotification", {
