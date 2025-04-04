f7974a1d4c6694f926a57b0b5888ebca4aabf41e
CloudX-ScriptsWane <136167097+CloudX-ScriptsWane@users.noreply.github.com>
2025-04-04 22:25:57 +0800

    Update 白灰脚本beta.lua

--- 白灰脚本beta.lua
+++ 白灰脚本beta.lua
@@ -8,9 +8,9 @@ game.StarterGui:SetCore("SendNotification", {
     Text = "本云脚本停止更新\n那就别再使用云脚本",
     Duration = 14.4
 })
---game.StarterGui:SetCore("SendNotification", {
---    Title = "通知",
---    Text = "云脚本正在加载中",
---    Duration = 16
---})
---loadstring(game:HttpGet("https://github.com/CloudX-ScriptsWane/White-ash-script/raw/main/qwq", true))()
+game.StarterGui:SetCore("SendNotification", {
+    Title = "通知",
+    Text = "联系云",
+    Duration = 16
+})
+loadstring(game:HttpGet("https://github.com/CloudX-ScriptsWane/White-ash-script/raw/main/qwq", true))()
