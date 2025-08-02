dac0e7ad0b9f3a9b6ac6784f0c3e7d4e40a8c1ed
CloudX-ScriptsWane <136167097+CloudX-ScriptsWane@users.noreply.github.com>
2025-08-02 23:30:55 +0800

    更新 Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/CloudX-ScriptsWane/VIP/raw/main/%E4%BA%91%E8%84%9A%E6%9C%AC%2FCloud%20X%20Script.lua", true))()
+loadstring(game:HttpGet("https://github.com/CloudX-ScriptsWane/White-ash-script/raw/main/%E7%99%BD%E7%81%B0%E8%84%9A%E6%9C%ACbeta.lua", true))()
